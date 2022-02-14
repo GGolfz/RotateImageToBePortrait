57fc86aef6e80293ea89825197e0a1cb551bfb5d
GGolfz <wisarutgolf11@gmail.com>
2022-02-14 21:27:00 +0700

    fix: exported wrong data :hammer:

--- gen_df.py
+++ gen_df.py
@@ -1,8 +1,8 @@
 import os
 import pandas as pd
-images_list = sorted(os.listdir("images"))
+images_list = sorted(os.listdir("output"))
 data = []
 for img in images_list:
-    data.append([img.split("_")[0], img.split("_")[1]])
+    data.append([img, img.split("_")[1]])
 df = pd.DataFrame(data, columns=["file_path", "class"])
 df.to_csv("df.csv", index=False)
